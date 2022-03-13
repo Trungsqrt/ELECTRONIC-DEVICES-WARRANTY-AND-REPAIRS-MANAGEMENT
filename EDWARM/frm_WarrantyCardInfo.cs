610dfd7c8e96ff8b945de3a7c7b1da713f736f1b
Trungsqrt <82458716+Trungsqrt@users.noreply.github.com>
2022-03-13 09:03:48 +0700

    Initial Forms

---
+++ EDWARM/frm_WarrantyCardInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EDWARM
+{
+    public partial class frm_WarrantyCardInfo : Form
+    {
+        public frm_WarrantyCardInfo()
+        {
+            InitializeComponent();
+        }
+    }
+}
