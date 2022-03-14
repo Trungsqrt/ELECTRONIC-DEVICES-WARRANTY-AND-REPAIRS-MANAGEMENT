448be31c41826b1f3465ad1e9de6a1cc6007a3d3
hnthinh55 <hnthinh55@gmail.com>
2022-03-14 09:55:40 +0700

    CDIO

---
+++ EDWARM/frm_ExportForCus.Designer.cs
@@ -0,0 +1,201 @@
+﻿
+namespace EDWARM
+{
+    partial class frm_ExportForCus
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lb = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txt_CardID = new System.Windows.Forms.TextBox();
+            this.txt_PriceOfWarranty = new System.Windows.Forms.TextBox();
+            this.txt_Note = new System.Windows.Forms.TextBox();
+            this.txt_DeliveryDate = new System.Windows.Forms.TextBox();
+            this.txt_CusID = new System.Windows.Forms.TextBox();
+            this.btn_ExportForCus = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(502, 56);
+            this.panel1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(114, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(265, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "WARRANTY CARD";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(71, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Card ID:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lb
+            // 
+            this.lb.AutoSize = true;
+            this.lb.Location = new System.Drawing.Point(12, 248);
+            this.lb.Name = "lb";
+            this.lb.Size = new System.Drawing.Size(118, 17);
+            this.lb.TabIndex = 6;
+            this.lb.Text = "Price of warranty:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(33, 199);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 17);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Delivery Date:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(41, 150);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(89, 17);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Customer ID:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(88, 302);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(42, 17);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Note:";
+            // 
+            // txt_CardID
+            // 
+            this.txt_CardID.Location = new System.Drawing.Point(136, 96);
+            this.txt_CardID.Name = "txt_CardID";
+            this.txt_CardID.Size = new System.Drawing.Size(227, 22);
+            this.txt_CardID.TabIndex = 10;
+            // 
+            // txt_PriceOfWarranty
+            // 
+            this.txt_PriceOfWarranty.Location = new System.Drawing.Point(136, 245);
+            this.txt_PriceOfWarranty.Name = "txt_PriceOfWarranty";
+            this.txt_PriceOfWarranty.Size = new System.Drawing.Size(227, 22);
+            this.txt_PriceOfWarranty.TabIndex = 11;
+            // 
+            // txt_Note
+            // 
+            this.txt_Note.Location = new System.Drawing.Point(136, 302);
+            this.txt_Note.Multiline = true;
+            this.txt_Note.Name = "txt_Note";
+            this.txt_Note.Size = new System.Drawing.Size(227, 115);
+            this.txt_Note.TabIndex = 13;
+            // 
+            // txt_DeliveryDate
+            // 
+            this.txt_DeliveryDate.Location = new System.Drawing.Point(136, 196);
+            this.txt_DeliveryDate.Name = "txt_DeliveryDate";
+            this.txt_DeliveryDate.Size = new System.Drawing.Size(227, 22);
+            this.txt_DeliveryDate.TabIndex = 14;
+            // 
+            // txt_CusID
+            // 
+            this.txt_CusID.Location = new System.Drawing.Point(136, 147);
+            this.txt_CusID.Name = "txt_CusID";
+            this.txt_CusID.Size = new System.Drawing.Size(227, 22);
+            this.txt_CusID.TabIndex = 15;
+            // 
+            // btn_ExportForCus
+            // 
+            this.btn_ExportForCus.Location = new System.Drawing.Point(390, 459);
+            this.btn_ExportForCus.Name = "btn_ExportForCus";
+            this.btn_ExportForCus.Size = new System.Drawing.Size(87, 35);
+            this.btn_ExportForCus.TabIndex = 16;
+            this.btn_ExportForCus.Text = "Export";
+            this.btn_ExportForCus.UseVisualStyleBackColor = true;
+            // 
+            // frm_ExportForCus
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(502, 506);
+            this.Controls.Add(this.btn_ExportForCus);
+            this.Controls.Add(this.txt_CusID);
+            this.Controls.Add(this.txt_DeliveryDate);
+            this.Controls.Add(this.txt_Note);
+            this.Controls.Add(this.txt_PriceOfWarranty);
+            this.Controls.Add(this.txt_CardID);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lb);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Name = "frm_ExportForCus";
+            this.Text = "frm_Export";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lb;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txt_CardID;
+        private System.Windows.Forms.TextBox txt_PriceOfWarranty;
+        private System.Windows.Forms.TextBox txt_Note;
+        private System.Windows.Forms.TextBox txt_DeliveryDate;
+        private System.Windows.Forms.TextBox txt_CusID;
+        private System.Windows.Forms.Button btn_ExportForCus;
+    }
+}
--- EDWARM/frm_WarrantyCardInfo.Designer.cs
+++ EDWARM/frm_WarrantyCardInfo.Designer.cs
@@ -28,12 +28,214 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btn_Search = new System.Windows.Forms.Button();
+            this.txt_CardID = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btn_Exit = new System.Windows.Forms.Button();
+            this.btn_Delete = new System.Windows.Forms.Button();
+            this.btn_Edit = new System.Windows.Forms.Button();
+            this.btn_ADD = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dgv_CardInformation = new System.Windows.Forms.DataGridView();
+            this.btn_Export = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_CardInformation)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1150, 56);
+            this.panel1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(419, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(265, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "WARRANTY CARD";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btn_Search);
+            this.groupBox1.Controls.Add(this.txt_CardID);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(0, 56);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1150, 81);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Search";
+            // 
+            // btn_Search
+            // 
+            this.btn_Search.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Search.Location = new System.Drawing.Point(452, 39);
+            this.btn_Search.Name = "btn_Search";
+            this.btn_Search.Size = new System.Drawing.Size(100, 31);
+            this.btn_Search.TabIndex = 1;
+            this.btn_Search.Text = "Search";
+            this.btn_Search.UseVisualStyleBackColor = true;
+            // 
+            // txt_CardID
+            // 
+            this.txt_CardID.Location = new System.Drawing.Point(235, 37);
+            this.txt_CardID.Name = "txt_CardID";
+            this.txt_CardID.Size = new System.Drawing.Size(175, 34);
+            this.txt_CardID.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(114, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Device_ID";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.btn_Exit);
+            this.flowLayoutPanel1.Controls.Add(this.btn_Export);
+            this.flowLayoutPanel1.Controls.Add(this.btn_Delete);
+            this.flowLayoutPanel1.Controls.Add(this.btn_Edit);
+            this.flowLayoutPanel1.Controls.Add(this.btn_ADD);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 468);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Padding = new System.Windows.Forms.Padding(100, 30, 0, 0);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1150, 100);
+            this.flowLayoutPanel1.TabIndex = 5;
+            // 
+            // btn_Exit
+            // 
+            this.btn_Exit.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Exit.Location = new System.Drawing.Point(947, 33);
+            this.btn_Exit.Name = "btn_Exit";
+            this.btn_Exit.Size = new System.Drawing.Size(100, 31);
+            this.btn_Exit.TabIndex = 5;
+            this.btn_Exit.Text = "Exit";
+            this.btn_Exit.UseVisualStyleBackColor = true;
+            // 
+            // btn_Delete
+            // 
+            this.btn_Delete.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Delete.Location = new System.Drawing.Point(735, 33);
+            this.btn_Delete.Name = "btn_Delete";
+            this.btn_Delete.Size = new System.Drawing.Size(100, 31);
+            this.btn_Delete.TabIndex = 4;
+            this.btn_Delete.Text = "Delete";
+            this.btn_Delete.UseVisualStyleBackColor = true;
+            // 
+            // btn_Edit
+            // 
+            this.btn_Edit.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Edit.Location = new System.Drawing.Point(629, 33);
+            this.btn_Edit.Name = "btn_Edit";
+            this.btn_Edit.Size = new System.Drawing.Size(100, 31);
+            this.btn_Edit.TabIndex = 3;
+            this.btn_Edit.Text = "Edit";
+            this.btn_Edit.UseVisualStyleBackColor = true;
+            // 
+            // btn_ADD
+            // 
+            this.btn_ADD.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_ADD.Location = new System.Drawing.Point(523, 33);
+            this.btn_ADD.Name = "btn_ADD";
+            this.btn_ADD.Size = new System.Drawing.Size(100, 31);
+            this.btn_ADD.TabIndex = 2;
+            this.btn_ADD.Text = "Add";
+            this.btn_ADD.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dgv_CardInformation);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(0, 137);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(1150, 331);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Result";
+            // 
+            // dgv_CardInformation
+            // 
+            this.dgv_CardInformation.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_CardInformation.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgv_CardInformation.Location = new System.Drawing.Point(3, 30);
+            this.dgv_CardInformation.Name = "dgv_CardInformation";
+            this.dgv_CardInformation.RowHeadersWidth = 51;
+            this.dgv_CardInformation.RowTemplate.Height = 24;
+            this.dgv_CardInformation.Size = new System.Drawing.Size(1144, 298);
+            this.dgv_CardInformation.TabIndex = 0;
+            // 
+            // btn_Export
+            // 
+            this.btn_Export.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Export.Location = new System.Drawing.Point(841, 33);
+            this.btn_Export.Name = "btn_Export";
+            this.btn_Export.Size = new System.Drawing.Size(100, 31);
+            this.btn_Export.TabIndex = 6;
+            this.btn_Export.Text = "Export";
+            this.btn_Export.UseVisualStyleBackColor = true;
+            // 
+            // frm_WarrantyCardInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1150, 568);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "frm_WarrantyCardInfo";
             this.Text = "frm_WarrantyCardInfo";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_CardInformation)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btn_Search;
+        private System.Windows.Forms.TextBox txt_CardID;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button btn_Exit;
+        private System.Windows.Forms.Button btn_Delete;
+        private System.Windows.Forms.Button btn_Edit;
+        private System.Windows.Forms.Button btn_ADD;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dgv_CardInformation;
+        private System.Windows.Forms.Button btn_Export;
     }
 }
